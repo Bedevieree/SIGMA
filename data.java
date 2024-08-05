2524d7b8160003ef63c7b038f88c46a5bff123e4
Bedevieree <evan_sutanto_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:42:10 +0700

    Add files via upload

---
+++ data.java
@@ -0,0 +1,12 @@
+public class data {
+    public static void main(String[] args) {
+        String nama = "skibidi";
+        int usia = 17 ;
+        double kelas = 10.5 ; 
+         System.out.println(nama+" sigma");
+         System.out.println(kelas+" mewing");
+         System.out.println(usia+"+");
+         System.out.println(nama+"");
+    }
+    
+}
