58df003fbb54f72953657bd688f4dc4dd6067b98
Bedevieree <evan_sutanto_33rpl@student.smktelkom-mlg.sch.id>
2024-08-09 14:26:43 +0700

    Add files via upload

---
+++ tester.java
@@ -0,0 +1,124 @@
+public class tester {
+
+    public static void main(String[] args) {
+
+        Siswa rawar = new Siswa();
+        Siswa okooks = new Siswa();
+        Siswa sma = new Siswa();
+        Siswa sd = new Siswa();
+        Siswa smp = new Siswa();
+
+        rawar.id = 1;
+        rawar.nama = "Arjuna";
+        rawar.ipk = 80.0;
+
+        System.out.println("nama =  " + rawar.nama);
+        System.out.println("id   =  " + rawar.id);
+        System.out.println("ipk  =  " + rawar.ipk);
+
+        okooks.id = 3;
+        okooks.nama = "ARYA";
+        okooks.ipk = 75.0;
+
+        System.out.println("nama =  "+ okooks.nama);
+        System.out.println("id   =  "+ okooks.id);
+        System.out.println("ipk  =  "+ okooks.ipk);
+   
+        sma.id = 10;
+        sma.nama = "Evan";
+        sma.ipk = 75.5;
+
+        System.out.println("nama =  "+ sma.nama);
+        System.out.println("id   =  "+ sma.id);
+        System.out.println("ipk  =  "+ sma.ipk);
+
+        sd.id = 19;
+        sd.nama = "Gilam";
+        sd.ipk = 80.0;
+
+        System.out.println("nama =  "+ sd.nama);
+        System.out.println("id   =  "+ sd.id);
+        System.out.println("ipk  =  "+sd .ipk);
+
+        smp.id = 20;
+        smp.nama = "Matthew";
+        smp.ipk = 10.0;
+
+        System.out.println("nama =  "+ smp.nama);
+        System.out.println("id   =  "+ smp.id);
+        System.out.println("ipk  =  "+ smp.ipk);
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    }
+
+}
